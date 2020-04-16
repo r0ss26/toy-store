dda02bc8214259ade70d4bad52d3bf81d7b4b052
Ross Baker <rossbaker32@gmail.com>
2020-04-16 16:44:16 +1000

    added link to individual toys

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -2,4 +2,12 @@ class ToysController < ApplicationController
   def index
     @toys = Toy.all
   end
+
+  def show
+    @toy = Toy.find_by_name(params[:toy])
+  end
+
+  def delete
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get("/toys", to: "toys#index")
+  get("/toys/:toy", to: "toys#show", as: "toy")
+
+  post("/toys/:toy/delete", to: "toys#delete", as: "delete")
 end
