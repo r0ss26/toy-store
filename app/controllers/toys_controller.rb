609d12105dc162f3a1cb0df51c881a4e06410944
Ross Baker <rossbaker32@gmail.com>
2020-04-18 11:51:23 +1000

    changed toy routes to ID's rather than names, in case of duplicate name

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -4,11 +4,16 @@ class ToysController < ApplicationController
   end
 
   def show
-    @toy = Toy.find_by_name(params[:toy])
+    @toy = Toy.find_by_id(params[:id])
   end
 
   def delete
     Toy.find_by_name(params[:toy]).destroy
-    redirect_to toys_path
+    redirect_to show_toys_path
+  end
+
+  def update
+    @toy = Toy.find_by_name(params[:toy]).destroy
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get("/toys", to: "toys#index", as: "toys")
-  get("/toys/:toy", to: "toys#show", as: "toy")
+  get("/toys", to: "toys#index", as: "show_toys")
+  get("/toys/:id", to: "toys#show", as: "show_toy")
 
-  delete("/toys/:toy", to: "toys#delete", as: "delete")
+  put("/toys/:toy", to: "toys#update", as: "update_toy")
+  patch("/toys/:toy", to: "toys#update")
+
+  delete("/toys/:toy", to: "toys#delete", as: "delete_toy")
 end
